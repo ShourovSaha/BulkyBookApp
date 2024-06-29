d13bbc76a9621a16521d91fee35cbe3318e78088
sh.saha <shourovsaha94@gmail.com>
2024-06-29 14:02:33 +0600

    Created Db connectionn (codefirst approach)

---
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public CategoryController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
